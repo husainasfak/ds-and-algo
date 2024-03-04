dd88e69c2190005e058269f77167e23464946740
husainasfak <asfakhusain99@gmail.com>
2024-03-04 10:29:20 +0530

    leetcode 647

--- CPP/Strings/11. Palindromic-substring.cpp
+++ CPP/Strings/11. Palindromic-substring.cpp
@@ -26,9 +26,37 @@ Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 #include <string>
 #include <cctype>
 using namespace std;
+// Brute force  -  Find all substring and check palindrome and increase the count - Time - O(n3);
+
+// Approach -  2: find odd length substring and check palindrome take O(n2) + find even length substring adn check Palindrome - O(n2) -> Total - O(N2)
+
+int expandAroundIndex(string s, int start, int end)
+{
+     int length = s.length();
+     int count = 0;
+     while (start >= 0 && end < length && s[start] == s[end])
+     {
+          count++;
+          start--;
+          end++;
+     }
+     return count;
+}
 
 int countSubstrings(string s)
 {
+     int count = 0;
+     int length = s.length();
+     for (int i = 0; i < length; i++)
+     {
+          // for odd legth staring;
+          int oddCount = expandAroundIndex(s, i, i);
+          count += oddCount;
+
+          int evenCount = expandAroundIndex(s, i, i + 1);
+          count += evenCount;
+     }
+     return count;
 }
 
 int main()
